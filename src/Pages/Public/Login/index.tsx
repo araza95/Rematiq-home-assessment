94537e6a9b9788c020674b1900a5a6ee90b4950d
araza95 <alihaiderizvi.you@gmail.com>
2025-03-11 16:30:32 +0100

    Migration step 1

--- src/Pages/Public/Login/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Fragment } from "react";
-interface ILoginProps {}
-
-const Login: React.FunctionComponent<ILoginProps> = () => {
-  return <Fragment></Fragment>;
-};
-
-export default Login;
